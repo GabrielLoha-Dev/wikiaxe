8b7ae675c26d1cb0b6bc62bd533df45c8d18b653
Loha <125302335+GabrielLoha-Dev@users.noreply.github.com>
2024-06-28 15:41:48 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,34 @@ function fadeInCards() {
         }, index * 200);
     });
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+    const elements = document.querySelectorAll(".inicio")
+    elements.forEach(element => {
+        element.classList.add("fade")
+    })
+})
+
+function filtrarCartoes() {
+    const input = document.getElementById('searchInput');
+    const filter = input.value.toLowerCase();
+    const cards = document.getElementsByClassName('card');
+    let found = false;
+
+    for (let i = 0; i < cards.length; i++) {
+        const title = cards[i].getElementsByTagName('h1')[0];
+        if (filter === '' || title.innerHTML.toLowerCase().indexOf(filter) > -1) {
+            cards[i].style.display = '';
+            found = true;
+        } else {
+            cards[i].style.display = 'none';
+        }
+    }
+
+    const notFoundMessage = document.getElementById('notFoundMessage');
+    if (!found) {
+        notFoundMessage.style.display = 'flex';
+    } else {
+        notFoundMessage.style.display = 'none';
+    }
+}
